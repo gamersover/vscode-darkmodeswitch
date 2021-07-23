147ba150df8914930519c92bbbcef6e4f3b94548
gamersover <cmathking@gmail.com>
2021-07-23 22:56:05 +0800

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,40 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let defaultTheme = vscode.workspace.getConfiguration("darkmodeswitch.defaultTheme");
+	let workbench = vscode.workspace.getConfiguration("workbench");
+	let isDark: boolean;
+
+	let disposable = vscode.commands.registerCommand('darkmodeswitch.switchtheme', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		let currColorTheme = vscode.window.activeColorTheme;
+		if (currColorTheme.kind === 2){
+			isDark = true;
+		}
+		else {
+			isDark = false;
+		}
+		if (isDark) {
+			workbench.update("colorTheme", defaultTheme.get("light"), true);
+			vscode.window.showInformationMessage("Switch to light mode");
+		}
+		else {
+			workbench.update("colorTheme", defaultTheme.get("dark"), true);
+			vscode.window.showInformationMessage("Switch to dark mode");
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
